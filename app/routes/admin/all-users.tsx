3690a9dc663ddc5a1e26a6c82ab7f7709817f70f
QuyetTV1991 <quyettv.bk@gmail.com>
2025-05-04 14:23:04 +0800

    trip card & stats card

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,15 @@
+import {Header} from "../../../components";
+
+const AllUser = () => {
+    return (
+        <main className="dashboard wrapper">
+            <Header
+                title="Users"
+                description="Check all current users in realtime"
+            />
+
+            All Users Page Content
+        </main>
+    )
+}
+export default AllUser
